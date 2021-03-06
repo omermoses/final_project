dbe89e0cb743b640918567230e60606fcf088f54
omer <omermoses17@gmail.com>
2021-03-06 12:03:16 +0200

    wip

--- export_data.py
+++ export_data.py
@@ -130,4 +130,3 @@ def write_clusters(clusters_kmeans, clusters_spectral, k_generated, k_used):
 # # k,s=jaccard_measure([1,1,2,2,0,1,1], [1,1,0,2,2,0,1], header)
 # # # # # # print(k, s)
 # create_pdf_file(samples, header, [1,1,2,2,0,0,1], [1,1,0,2,2,0,1] ,3, 3, 7)
-
