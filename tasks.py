998638dcb45de723d839e465ecccffa8c08e14d4
gay-wolf <gaywolf@mail.tau.ac.il>
2021-03-11 19:23:58 +0200

    update jaccard, change k when Random=False, run in task, args.Random

--- tasks.py
+++ tasks.py
@@ -15,4 +15,7 @@ def build(c):
 @task(pre=[build], post=[delete])
 def run(c, k, n, Random=True):
     print("start running")
-    c.run("python3.8.5 main.py {} {} {}".format(k, n, Random))
+    if Random:
+        c.run("python3.8.5 main.py {} {}".format(k, n))
+    else:
+        c.run("python3.8.5 main.py {} {} --Random".format(k, n))
